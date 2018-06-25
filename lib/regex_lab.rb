922aadc270fe6641650fb14aa8c194f45ddfc901
Marissa Zhong <mwz2101@columbia.edu>
2018-06-25 16:44:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,7 +18,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.length === 5
+  text.length === 5 #assume no trailing or leading spaces or punctuation?
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
