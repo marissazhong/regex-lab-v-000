3542a186cfb219fc3ec0e1769caa167d4e3a139f
Marissa Zhong <mwz2101@columbia.edu>
2018-06-25 17:26:02 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,12 +8,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\Aun/)
+  text.scan(/\bun[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
   matches = /[[:alpha:]]/.scan(text)
-  matches.length === 5
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
